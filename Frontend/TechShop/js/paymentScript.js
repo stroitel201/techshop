7a20d4c857b4128d13754bed0d2e6d2b9a290536
stroitel <oleg.vizgalov@gmail.com>
2020-11-19 23:07:04 +0300

    payment

---
+++ Frontend/TechShop/js/paymentScript.js
@@ -0,0 +1,25 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".input").focus(function () {
+    let label = $(this).parent().find(".label-txt");
+    label.addClass("label-active");
+    label.css("color", "rgb(120, 120, 120)");
+  });
+
+  $(".input").focusout(function () {
+    if ($(this).val() == "") {
+      let label = $(this).parent().find(".label-txt");
+      label.removeClass("label-active");
+    }
+  });
+
+  document.querySelector("#payButton").addEventListener("click", () => {
+    let arr = document.querySelectorAll(".input");
+    console.log(arr);
+    arr.forEach((element) => {
+      if (element.value === "")
+        element.parentNode.querySelector(".label-txt").style.color = "#b00000";
+    });
+  });
+});
